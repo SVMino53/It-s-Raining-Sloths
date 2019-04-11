1c7f127bf9917ed31824f719360a9950517a3b67
Marina Kostrova <kostrovmarinaa@yandex.ru>
2019-04-11 14:32:26 +0200

    Movement and rotation

--- It's Raining Sloths/Assets/Scripts/Test/Rotate.cs
+++ It's Raining Sloths/Assets/Scripts/Test/Rotate.cs
@@ -4,29 +4,60 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    public KeyCode RotateLeft;
-    public KeyCode RotateRight;
-    public float RotationSpeed;
+    [SerializeField]
+    KeyCode RotateLeft;
+    [SerializeField]
+    KeyCode RotateRight;
+    [SerializeField]
+    float RotationSpeed;
+    [SerializeField]
+    int numberOfLanes;
+    float rotationAngle;
+    
+    //easing
+    float x = 0.0f;
+    bool up = true;
+
+    public float curSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rotationAngle = 360 / numberOfLanes;
     }
 
     // Update is called once per frame
     void Update()
     {
         // Make it lane based!!!
-
-        if(Input.GetKey(RotateLeft))
+        
+        if(Input.GetKeyDown(RotateLeft))
         {
-            transform.Rotate(0, RotationSpeed, 0);
+            StartCoroutine(ChangeLane(-RotationSpeed));
         }
 
-        if(Input.GetKey(RotateRight))
+        if(Input.GetKeyDown(RotateRight))
         {
-            transform.Rotate(0, -RotationSpeed, 0);
+            StartCoroutine(ChangeLane(RotationSpeed));
         }
     }
+
+    IEnumerator ChangeLane(float speed)
+    {
+        float curAngel = 0;
+        while(curAngel < rotationAngle)
+        {
+            transform.Rotate(0, speed, 0);
+            curAngel += Mathf.Abs(speed);
+            yield return null;
+        }
+    }
+
+    /*float GetCurSpeed()
+    {
+        x += 0.01f;
+        if (x >= 2*Mathf.PI) x = 0;
+        //if (Mathf.Sin(x) <= RotationSpeed/2)
+        return Mathf.Sin(x/2);
+    }*/
 }
