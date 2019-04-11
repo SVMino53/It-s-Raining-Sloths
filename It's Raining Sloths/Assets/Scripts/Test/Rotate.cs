18cbf7b3bf72c817792b356f5832fc4c0fda42db
Isak Forsberg <isak-forsberg@hotmail.se>
2019-04-11 11:29:49 +0200

    Rotating

---
+++ It's Raining Sloths/Assets/Scripts/Test/Rotate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public KeyCode RotateLeft;
+    public KeyCode RotateRight;
+    public float RotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Make it lane based!!!
+
+        if(Input.GetKey(RotateLeft))
+        {
+            transform.Rotate(0, RotationSpeed, 0);
+        }
+
+        if(Input.GetKey(RotateRight))
+        {
+            transform.Rotate(0, -RotationSpeed, 0);
+        }
+    }
+}
