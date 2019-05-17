2260791b69f7314667778759015b9e9665faca41
Marina Kostrova <kostrovmarinaa@yandex.ru>
2019-05-17 13:41:11 +0200

    Main menu + video

---
+++ It's Raining Sloths/Assets/StartScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScript : MonoBehaviour
+{
+    [SerializeField]
+    string MainSceneName = "Video";
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown || Input.GetAxis("Mouse Y") < 0)
+        {
+            SceneManager.LoadScene(MainSceneName);
+        }
+    }
+}
