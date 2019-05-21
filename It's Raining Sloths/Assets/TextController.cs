5588d1233d5f7c67056f877b48d5c76f07ab7745
Marina Kostrova <kostrovmarinaa@yandex.ru>
2019-05-21 16:33:27 +0200

    Tutorial

--- It's Raining Sloths/Assets/TextController.cs
+++ It's Raining Sloths/Assets/TextController.cs
@@ -11,15 +11,26 @@ public class TextController : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField]
     TextMeshProUGUI ClimbText;
+    [SerializeField]
     string ClimbStr = "Move mat to climb!";
     [SerializeField]
     TextMeshProUGUI RotateText;
-    string RotateStr = "Balance to rotate!";
+    [SerializeField]
+    string RotateStr = "Balance to rotate and avoid branches! Don't hit them twice";
     [SerializeField]
     TextMeshProUGUI CatchText;
-    string CatchStr = "Press buttons to catch babies!";
+    [SerializeField]
+    string CatchStr = "Press buttons to catch the babies!";
     [SerializeField]
     float delayTime = 2f;
+
+    [SerializeField]
+    GameObject rockPrefab;
+    [SerializeField]
+    GameObject rockSpawner;
+
+    GameObject player; 
+    
 
     bool climbFinished = false;
     bool rotateFinished = false;
@@ -28,6 +39,8 @@ public class TextController : MonoBehaviour
     void Start()
     {
         ClimbText.text = ClimbStr;
+        player = GameObject.Find("Player");
+        player.GetComponent<Rotate_Analog>().enabled = false;
     }
 
     // Update is called once per frame
@@ -52,27 +65,11 @@ public class TextController : MonoBehaviour
 
         if(CatchText.text == CatchStr)
         {
-            if ((Input.GetKeyDown(KeyCode.F) || Input.GetKeyDown(KeyCode.G)) && GameObject.Find("Player").GetComponent<Score>().GetScore() > 0)
+            if ((Input.GetKeyDown(KeyCode.F) || Input.GetKeyDown(KeyCode.G)) && player.GetComponent<Score>().GetScore() > 0)
             {
                 StartCoroutine(Wait(CatchStr));
             }
         }
-        
-        if (catchFinished)
-        {
-            CatchText.text = "";
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
-        else if (rotateFinished)
-        {
-            RotateText.text = "";
-            CatchText.text = CatchStr;
-        }
-        else if(climbFinished)
-        {
-            ClimbText.text = "";
-            RotateText.text = RotateStr;
-        } 
     }
 
     IEnumerator Wait(string smth)
@@ -82,11 +79,34 @@ public class TextController : MonoBehaviour
         if (smth == ClimbStr) climbFinished = true;
         else if (smth == RotateStr) rotateFinished = true;
         else if (smth == CatchStr) catchFinished = true;
+        Reset();
     }
 
-    IEnumerator boolLogic()
+    private void Reset()
     {
-        
-        yield return null;
-    } 
+        if (catchFinished)
+        {
+            CatchText.text = "";
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        else if (rotateFinished)
+        {
+            RotateText.text = "";
+            CatchText.text = CatchStr + " " + player.GetComponent<Score>().GetScore() +"/2";
+            GameObject.Find("Systems").GetComponent<SpawnSlothBaby>().enabled = true;
+            player.GetComponent<GeneralMovement>().moving = true;
+        }
+        else if (climbFinished)
+        {
+            ClimbText.text = "";
+            RotateText.text = RotateStr;
+            player.GetComponent<Rotate_Analog>().enabled = true;
+            player.GetComponent<GeneralMovement>().moving = false;
+            if (rockPrefab)
+            {
+                Instantiate<GameObject>(rockPrefab, rockSpawner.transform.position, rockSpawner.transform.rotation);
+                rockPrefab = null;
+            }
+        }
+    }
 }
